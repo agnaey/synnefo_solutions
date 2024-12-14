8b7c67d7fbbcf49b24fa98f85db834a75a445c0c
agnaey <agnaeyviswanath1234@gmail.com>
2024-12-14 12:03:48 +0530

    course carousel added

--- synnefo/app/views.py
+++ synnefo/app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Courses
 
 
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    course=Courses.objects.all()[:4]
+    course1=Courses.objects.all()[4:8]
+    return render(request,'index.html',{'course':course,'course1':course1})
